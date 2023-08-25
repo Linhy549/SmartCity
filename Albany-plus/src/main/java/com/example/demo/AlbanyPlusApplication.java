1d215113c09491e46a3fb5029d1ac62ca40cc6e7
Linhy549 <1098313754@qq.com>
2023-08-25 08:18:51 -0400

    added

---
+++ Albany-plus/src/main/java/com/example/demo/AlbanyPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbanyPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbanyPlusApplication.class, args);
+	}
+
+}
